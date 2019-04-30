c079e44c749b12720f272fce860ece918ffbeb83
Wushnedge <b7018846@my.shu.ac.uk>
2019-04-30 18:06:34 +0100

    Update LevelBuilder.cpp
    Changed arrays to have the exact amount of coordinates

--- Folder/CollisionSOL/LevelBuilder.cpp
+++ Folder/CollisionSOL/LevelBuilder.cpp
@@ -14,7 +14,7 @@ using namespace DirectX::SimpleMath;
 					//-3.5f, -3.5f, -3.5f, -3.5f};
 //float cubeY[34] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5,1,1.5f,2,2,2,2,2,2,2,2,2,2,2,2,2,1.5f,1,0.5f };
 
-float cubeX[200] = {-3.5f, -3, -2.5f, -2, -1.5f, -1, -0.5f, 0, 0.5f, 1, 1.5f, 2, 2.5f, 3, 3.5f, 3.5f,
+float cubeX[125] = {-3.5f, -3, -2.5f, -2, -1.5f, -1, -0.5f, 0, 0.5f, 1, 1.5f, 2, 2.5f, 3, 3.5f, 3.5f,
 					3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 
 					3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3, 2.5f, 2, 1.5f, 1, 0.5f, 0, -0.5f, -1, -1.5f, -2,
 					-2.5f, -3, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, -3.5f, 
@@ -23,7 +23,7 @@ float cubeX[200] = {-3.5f, -3, -2.5f, -2, -1.5f, -1, -0.5f, 0, 0.5f, 1, 1.5f, 2,
 					2, 2, 2, 2, 2, 2, 2, -3, -2.5f, -2, -1.5f, -1, -0.5f, 0, 0.5f, 1, 1.5f, 2.5f, -3,
 					-2.5f, -2, -1.5f, -1, -0.5f, 0, 0.5, 3, -2.5f, 0, 0, 0, 0, 0};
 
-float cubeY[200] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5f, 1, 1.5f, 2, 2.5f, 3, 3.5f, 4,
+float cubeY[125] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5f, 1, 1.5f, 2, 2.5f, 3, 3.5f, 4,
 					4.5f, 5, 5.5f, 6, 6.5f, 7, 7.5f, 8, 8.5f, 9, 9.5f, 10, 10, 10, 10, 10, 10, 10, 10, 
 					10, 10, 10, 10, 10, 10, 10, 9.5f, 9, 8.5f, 8, 7.5f, 7, 6.5f, 6, 5.5f, 5, 4.5f, 4,
 					3.5f, 3, 2.5f, 2, 1.5f, 1, 0.5f, 9.5f, 9, 8.5f, 8, 7.5f, 6, 6, 6, 6, 6, 6.5f, 7,
@@ -37,7 +37,7 @@ void LevelBuilder::Initialise(MeshManager& mgr)//, int levelSize)
 	cubies.Initialise(*p, 1, 1);
 
 	//Sets up the array for the cubes that make up the level
-	for (int i = 0; i < 200; i++) {
+	for (int i = 0; i < 125; i++) {
 		cubies.Initialise(*p, cubeX[i], cubeY[i]);
 		mCubes[i] = cubies.GetCube();
 		//Puts the the cube in the array
@@ -46,7 +46,7 @@ void LevelBuilder::Initialise(MeshManager& mgr)//, int levelSize)
 
 void LevelBuilder::Render(FX::MyFX& fx)
 {
-	for (int i = 0; i < 200; i++) {
+	for (int i = 0; i < 125; i++) {
 		fx.Render(mCubes[i], gd3dImmediateContext);
 	}
 }
